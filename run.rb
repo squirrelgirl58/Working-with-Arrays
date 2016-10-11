23a19c9a2b3cc1222009574e24ecd373f483900a
“squirrelgirl58” <kdillon2@nd.edu>
2016-10-11 01:48:25 -0400

    git assignment 1

--- run.rb
+++ run.rb
@@ -8,14 +8,18 @@ get '/' do
 end
 
 get '/years' do
-
-
-
   # code!
+  @years_para = 'I have been alive for 21 years.'
+  # @years = (1995..2016).to_a
+  @years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
   erb :years, layout: :main
 end
 
 get '/states' do
   # code!
+  @states_para = 'I have visited the following states:'
+  @states = ["maryland", "california", "illinois", "indiana", "washington"]
+  @states.sort
+  @states.sort!
   erb :states, layout: :main
 end
